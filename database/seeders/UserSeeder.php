b29e3e4c80ed43b1f516fc05f90c7299191ccfce
Bangkit Juang Raharjo <bangkitjuangraharjo@gmail.com>
2023-09-06 23:49:28 +0700

    pembuatan controller untuk login

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,9 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class UserSeeder extends Seeder
 {
@@ -13,6 +14,11 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        User::create([
+            "name" => "Bangkit Juang Raharjo",
+            "email" => "bangkitjuangraharjo@gmail.com",
+            "phone_number" => "+6281325507780",
+            "password" => Hash::make("Juang Tampan")
+        ]);
     }
 }
