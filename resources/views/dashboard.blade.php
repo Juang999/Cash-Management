144dd4a933bc6040c262dd0d78f0dc00b797421e
Bangkit Juang Raharjo <bangkitjuangraharjo@gmail.com>
2023-09-08 01:37:21 +0700

    new page for dashboard and fix template

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,42 @@
+@extends('template.template')
+
+@section('title', 'Dashboard')
+
+@push('css')
+    <link rel="stylesheet" href="{{asset('css/dashboard.css')}}">
+@endpush
+
+@section('content')
+<div class="container h-100 d-flex justify-content-evenly align-items-center">
+    <div class="flex-item">
+        <div class="card d-flex flex-column" style="width: 18rem;">
+            <div class="bg-icon d-flex justify-content-center align-items-center w-100">
+                <img src="{{ asset('images/icon/grace-period.png') }}" class="card-img-top img-icon flex-item">
+            </div>
+            <div class="card-body d-flex justify-content-center">
+                <a href="/periode" class="btn fs-5 nav-link">Periode</a>
+            </div>
+        </div>
+    </div>
+    <div class="flex-item">
+        <div class="card d-flex flex-column" style="width: 18rem;">
+            <div class="bg-icon d-flex justify-content-center align-items-center w-100">
+                <img src="{{ asset('images/icon/save-money.png') }}" class="card-img-top img-icon flex-item">
+            </div>
+            <div class="card-body d-flex justify-content-center">
+                <a href="#" class="btn fs-5 nav-link">Cash Management</a>
+            </div>
+        </div>
+    </div>
+    <div class="flex-item">
+        <div class="card d-flex flex-column" style="width: 18rem;">
+            <div class="bg-icon d-flex justify-content-center align-items-center w-100">
+                <img src="{{ asset('images/icon/wish-list.png') }}" class="card-img-top img-icon flex-item">
+            </div>
+            <div class="card-body d-flex justify-content-center">
+                <a href="#" class="btn fs-5 nav-link">Wishlist</a>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- resources/views/template/template.blade.php
+++ resources/views/template/template.blade.php
@@ -4,13 +4,46 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>@yield('title') | Cash Management</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/template.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/navbar.css') }}">
+    @stack('css')
 </head>
 
 <body>
-    <h1>Hello, world!</h1>
+    <nav class="navbar bg-primary navbar-expand-lg">
+        <div class="container-fluid">
+            <a class="navbar-brand text-white" href="/dashboard">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav w-100 d-flex justify-content-between">
+                    <li class="nav-item flex-item">
+                        {{-- <a class="nav-link active text-white" aria-current="page" href="#">Home</a> --}}
+                    </li>
+                    <li class="nav-item dropdown flex-item d-flex">
+                        <img src="{{ asset('images/icon/user.png') }}" id="profile-photo" alt="">
+                        <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            Bangkit Juang R
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Profile</a></li>
+                            <li><a class="dropdown-item" href="#">Logout</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="content">
+        @yield('content')
+    </div>
+    @stack('jquery')
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
     </script>
