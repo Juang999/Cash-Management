8dbc028c4790485e00518ff2bb92ee54a4a050bf
Bangkit Juang Raharjo <bangkitjuangraharjo@gmail.com>
2023-09-08 01:38:02 +0700

    add new route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('login');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+
+Route::get('/periode', function () {
+    return view('periode');
+});
