1a4e6b75679562d468c1242bef7305f6dff4a641
Bangkit Juang Raharjo <bangkitjuangraharjo@gmail.com>
2023-09-12 06:21:30 +0700

    new feature for login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,10 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\AuthRequest;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function login() {
-        
+    public function login(Request $request) {
+        $credentiasl = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt($credentiasl)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('dashboard');
+        }
+
+        return back()->withErrors([
+            'email' => 'email atau password salah!'
+        ])->onlyInput(['email', 'password']);
     }
 }
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -14,7 +14,7 @@
     <div class="flex-item">
         <div class="card mb-3" style="max-width: 540px;">
             <div class="row g-0">
-                <div class="col-md-4 d-flex justify-content-center align-items-center">
+                <div class="col-md-4 d-flex justify-content-center align-items-center border-end">
                     <div class="div-icon d-flex justify-content-center align-items-center">
                         <img src="{{asset("images/icon/save-money.png")}}" class="img-fluid rounded-start icon-login">
                     </div>
@@ -22,15 +22,16 @@
                 <div class="col-md-8">
                     <div class="card-body">
                         <h5 class="card-title">Login</h5>
-                        <form>
+                        <form method="POST" action="/auth/login">
+                            @csrf
                             <div class="mb-3">
                                 <label for="exampleInputEmail1" class="form-label card-text fs-6">Email address</label>
-                                <input type="email" class="form-control" id="exampleInputEmail1"
+                                <input type="email" name="email" class="form-control" id="exampleInputEmail1"
                                     aria-describedby="emailHelp">
                             </div>
                             <div class="mb-3">
                                 <label for="exampleInputPassword1" class="form-label card-text fs-6">Password</label>
-                                <input type="password" class="form-control" id="exampleInputPassword1">
+                                <input type="password" name="password" class="form-control" id="exampleInputPassword1">
                             </div>
                             <button type="submit" class="btn btn-bg text-light">Submit</button>
                         </form>
